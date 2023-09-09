2841d6fdc6401b24a61f19e6a8f8f0b3df5e9df3
Amey Tendulkar <tendulkaramey.git@gmail.com>
2023-09-09 17:04:06 +0530

    models added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+def default_json():
+    return {}
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=now, editable=False)
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=now, editable=False)
+
+class Product(models.Model):
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.FloatField(null=False)
+    description = models.TextField(default='')
+    stock = models.IntegerField(default=0)
+    rating = models.FloatField(null=True)
+    reviews = models.JSONField(default=default_json)
+    additional_info = models.JSONField(default=default_json)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=now, editable=False)
--- globalmart/settings.py
+++ globalmart/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api"
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,15 @@ WSGI_APPLICATION = "globalmart.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "globalmart",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": '5432',
     }
 }
+
 
 
 # Password validation
